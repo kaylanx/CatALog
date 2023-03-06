a02685503fb596d43c102e018918c21b806561f5
Andy Kayley <bitbucket@kayley.name>
2023-03-06 19:16:49 +0000

    Initial Commit

---
+++ CatALog/CatALogApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatALogApp.swift
+//  CatALog
+//
+//  Created by Andy Kayley on 06/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct CatALogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CatALog/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  CatALog
+//
+//  Created by Andy Kayley on 06/03/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
